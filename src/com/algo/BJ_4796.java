f0fc9e53efc7bd682a5d8265d7f58dd89ebd27c5
am003507 <am003507@gmail.com>
2019-06-29 19:33:01 +0900

    완료

--- src/com/algo/BJ_4796.java
+++ src/com/algo/BJ_4796.java
@@ -1,7 +1,25 @@
 package com.algo;
 
-public class BJ_4796 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class BJ_4796 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        int i =1;
+        while (true){
+            st = new StringTokenizer(bf.readLine());
+            int L = Integer.parseInt(st.nextToken());
+            int P = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+            if( L == 0 && P==0 && V ==0){
+                break;
+            }
+            int ret  = (V/P)*L +((V%P) > L ? L :(V%P));
+            System.out.println("Case "+ i++ +": "+ret);
+        }
     }
 }
